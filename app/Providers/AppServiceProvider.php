7f6ed4c16f1b52370cc208a0265a6617bdce5e45
Tobias <tobias.sanchezdev@gmail.com>
2026-08-03 13:42:20 -0300

    Fix: forzar HTTPS en los estilos CSS para Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 // 1. Agrega esta importación en la parte superior
 use Illuminate\Pagination\Paginator; 
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,5 +18,10 @@ class AppServiceProvider extends ServiceProvider
     {
         // 2. Agrega esta línea para forzar el uso de Bootstrap
         Paginator::useBootstrapFive();
+
+        // protección de HTTPS para Vercel
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
